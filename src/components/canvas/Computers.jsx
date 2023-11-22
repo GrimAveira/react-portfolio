22f1cbb3e8af6e18722fdc0cd6ce57267eb9b685
GrimAveira <st.tryed@gmail.com>
2023-11-22 15:19:39 +0400

    Modified hero component Added     - Hero.jsx now has animated scroller     - Computers.jsx now has resize 3d model for small devices Fixed     - Loader.jsx now implements progress in percent while 3d model is loading

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,19 +3,41 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={3} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.45 : 0.7}
+        position={[0, -3.25, -1.25]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
@@ -29,7 +51,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
